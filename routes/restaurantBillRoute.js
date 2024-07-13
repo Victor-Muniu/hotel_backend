aaf2ce5ad85fe0d71b25886009b7a433564b4ece
Victor-Muniu <victornjoroge2020@gmail.com>
2024-07-13 12:29:07 +0300

    done

--- routes/restaurantBillRoute.js
+++ routes/restaurantBillRoute.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const RestaurantBill =require('../sales/restaurantBills')
+const Table = require('../models/table')
 router.get('/restaurantBills', async (req, res) => {
     try {
         const bills = await RestaurantBill.find().populate('restaurantOrderId');
@@ -34,16 +35,26 @@ router.patch('/restaurantBills/:id', async (req, res) => {
     const updates = req.body;
 
     try {
-        const updatedBill = await RestaurantBill.findByIdAndUpdate(id, updates, { new: true });
+        const updatedBill = await RestaurantBill.findByIdAndUpdate(id, updates, { new: true }).populate('restaurantOrderId');
 
         if (!updatedBill) {
             return res.status(404).json({ message: 'Restaurant bill not found' });
+        }
+        if (updatedBill.status === 'Cleared') {
+        
+            const table = await Table.findById(updatedBill.restaurantOrderId.tableId);
+
+            if (table) {
+                
+                table.status = 'Available';
+                await table.save();
+            }
         }
 
         res.json(updatedBill);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
-})
+});
 
 module.exports =router
