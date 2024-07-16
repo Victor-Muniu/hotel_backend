61034a12c6ac93966f3d1d0761566178c138e0a1
Victor-Muniu <victornjoroge2020@gmail.com>
2024-07-16 16:07:38 +0300

    done

--- routes/creditorsRoute.js
+++ routes/creditorsRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Creditors = require('../accounts/creditors');
-
+const TrialBalance = require('../accounts/trial_balance')
 
 router.post('/creditors', async (req, res) => {
     try {
@@ -12,6 +12,19 @@ router.post('/creditors', async (req, res) => {
             date,
             amount
         });
+        let trialBalanceEntry = await TrialBalance.findOne({ group_name: 'Debtors', Date: { $gte: new Date(currentYear, 0, 1), $lt: new Date(currentYear + 1, 0, 1) } });
+        if (!trialBalanceEntry) {
+            trialBalanceEntry = new TrialBalance({
+                group_name: 'Credit',
+                Debit: 0,
+                Credit: amount,
+                Date: new Date()
+            });
+        } else {
+            trialBalanceEntry.Credit += amount;  
+        }
+    
+        await trialBalanceEntry.save();
 
         await newCreditor.save();
         res.status(201).json(newCreditor);
